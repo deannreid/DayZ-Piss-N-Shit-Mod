5dbf53bf0f65f4f53040cb98ddaddeeb91dfc2cb
Dean Reid <deannreid@hotmail.co.uk>
2023-04-16 19:40:31 +0100

    Revert "Removed Unrequired Modifiers"
    This reverts commit 69e2a08cd1f6fffeb9d022dd4892756cb1cbb2a0.

--- Game Files/@PissNShit/Addons/PNS_Core/Scripts/4_World/classes/playermodifiers/modifiermanager.c
+++ Game Files/@PissNShit/Addons/PNS_Core/Scripts/4_World/classes/playermodifiers/modifiermanager.c
@@ -15,16 +15,14 @@
  * 05/12/2022 - Initial Code Development
  */
 
-//Save the extra file
-enum eModifiersPNS {
-	MDF_BLADDER = 1337,
-	MDF_BOWEL,
-}
-
 modded class ModifiersManager {
+
 	override void Init () {
-		super.Init(); 
+		super.Init();
+		 
 		AddModifier  ( new BladderMdfr );
-		AddModifier  ( new BowelMdfr );
+	//	AddModifier  ( new BowelMdfr );
+	
 	}
+
 }
