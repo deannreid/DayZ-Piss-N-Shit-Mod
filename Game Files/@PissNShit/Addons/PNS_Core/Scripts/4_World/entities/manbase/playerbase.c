bd42e2a03d6ea8eac5c295e20a0d0962af61a480
Dean Reid <deannreid@hotmail.co.uk>
2023-04-30 15:54:15 +0100

    Possible Fix for Stats

--- Game Files/@PissNShit/Addons/PNS_Core/Scripts/4_World/entities/manbase/playerbase.c
+++ Game Files/@PissNShit/Addons/PNS_Core/Scripts/4_World/entities/manbase/playerbase.c
@@ -22,6 +22,32 @@ modded class PlayerBase extends ManBase
 	protected PlayerStat<float> m_StatBladder;
 	protected PlayerStat<float> m_StatBowel;
 		
+	EStatLevels GetStatLevelBladder() {
+		float bladder = GetStatBladder().Get();
+		return GetStatLevel(bladder, PlayerConstants.SL_BLADDER_CRITICAL, PlayerConstants.SL_BLADDER_LOW, PlayerConstants.SL_BLADDER_NORMAL, PlayerConstants.SL_BLADDER_HIGH);
+		Print("[PNS :: DEBUG] -- PlayerBase [BLADDER] : GetStatLevelBladder Current State:" +bladder);
+	}
+
+	EStatLevels GetStatLevelBowel() {
+		float bowel = GetStatBowel().Get();
+		return GetStatLevel(bowel, PlayerConstants.SL_BOWEL_CRITICAL, PlayerConstants.SL_BOWEL_LOW, PlayerConstants.SL_BOWEL_NORMAL, PlayerConstants.SL_BOWEL_HIGH);
+		Print("[PNS :: DEBUG] -- PlayerBase [BLADDER] : GetStatLevelBowel Current State:" +bowel);
+	}
+
+	float GetStatBordersBladder()
+	{
+		float bladder 		= GetStatBladder().Get();
+		float bladder_max 	= GetStatBladder().GetMax();
+		return GetStatLevelBorders(bladder, PlayerConstants.SL_BLADDER_CRITICAL, PlayerConstants.SL_BLADDER_LOW, PlayerConstants.SL_BLADDER_NORMAL, PlayerConstants.SL_BLADDER_HIGH, bladder_max);
+	}	
+
+	float GetStatBordersBowel()
+	{
+		float bowel 	= GetStatBowel().Get();
+		float bowel_max = GetStatBowel().GetMax();
+		return GetStatLevelBorders(bowel, PlayerConstants.SL_BOWEL_CRITICAL, PlayerConstants.SL_BOWEL_LOW, PlayerConstants.SL_BOWEL_NORMAL, PlayerConstants.SL_BOWEL_HIGH, bowel_max);
+	}	
+	
 	PlayerStat<float> GetStatBladder() {
 		if( !m_StatBladder && GetPlayerStats()) {
 			m_StatBladder = PlayerStat<float>.Cast(GetPlayerStats().GetStatObject(EPlayerStats_PNSMod.BLADDER));
@@ -36,17 +62,5 @@ modded class PlayerBase extends ManBase
 		}
 		return m_StatBowel;
 		Print("[PNS :: DEBUG] -- PlayerBase [BLADDER] : GetStatBowel Current State:" +m_StatBowel);
-	}	
-
-	EStatLevels GetStatLevelBladder() {
-		float bladder = GetStatBladder().Get();
-		return GetStatLevel(bladder, PlayerConstants.SL_BLADDER_CRITICAL, PlayerConstants.SL_BLADDER_LOW, PlayerConstants.SL_BLADDER_NORMAL, PlayerConstants.SL_BLADDER_HIGH);
-		Print("[PNS :: DEBUG] -- PlayerBase [BLADDER] : GetStatLevelBladder Current State:" +bladder);
-	}
-
-	EStatLevels GetStatLevelBowel() {
-		float bowel = GetStatBowel().Get();
-		return GetStatLevel(bowel, PlayerConstants.SL_BOWEL_CRITICAL, PlayerConstants.SL_BOWEL_LOW, PlayerConstants.SL_BOWEL_NORMAL, PlayerConstants.SL_BOWEL_HIGH);
-		Print("[PNS :: DEBUG] -- PlayerBase [BLADDER] : GetStatLevelBowel Current State:" +bowel);
-	}
+	}		
 }
